fdb1a072981faf898d04db27891f7601093aac28
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-07 22:47:30 +0100

    -

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 `use strict`;
+
+// DOM - Document Object Model - connection point between js and other files. so html file with a button that links to js
+// DOM - document.queryselector()
+
+/* 
+// DOM manipulation - changing values
+console.log(document.querySelector(`.message`).textContent); // outputs the text in console
+document.querySelector(`.number`).textContent = 13; // changes the ID to whatever i tell it too
+document.querySelector(`.guess`).value = 7;
+ */
+
+// event listener basicly waits for an input via keypress, buttonclick, scroll, mouse movement ect.
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  // when check button is clicked, print the inputed value
+  console.log(document.querySelector(`.guess`).value);
+});
