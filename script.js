d10b60da33322b7a394a45236cc314dec1725d7f
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-09 22:58:20 +0100

    text content error!!

--- script.js
+++ script.js
@@ -10,14 +10,68 @@ document.querySelector(`.number`).textContent = 13; // changes the ID to whateve
 document.querySelector(`.guess`).value = 7;
  */
 
+// random number generator
+// .random is between 0 and 1, so if timesed by 20 it will be between 0 and 20
+//trunc truncates it into an intager. but that means 20 is impossible cos 19.9999999 so just add 1
+const maxNamber = 1;
+let ranNumber = Math.trunc(Math.random() * maxNamber) + 1;
+let score = maxNamber;
+document.querySelector(`.score`).textContent = score;
+//console.log(ranNumber);
+//document.querySelector(`.number`).textContent = ranNumber;
+
 // event listener basicly waits for an input via keypress, buttonclick, scroll, mouse movement ect.
+// when check button is clicked, print the inputed value
+
 document.querySelector(`.check`).addEventListener(`click`, function () {
-  // when check button is clicked, print the inputed value
-  console.log(document.querySelector(`.guess`).value);
+  const guess = Number(document.querySelector(`.guess`).value);
 
-  console.log(guess, typeof guess);
+  console.log(` Input: ${document.querySelector(`.guess`).value}`);
+  console.log(`${guess} is a ${typeof guess}`);
 
+  // when there is no input
   if (!guess) {
-    document.querySelector(`.message`).textContent = `No Number!`;
+    document.querySelector(`.message`).textContent = `No Number Input`;
+
+    // when there is a win
+  } else if (guess === ranNumber) {
+    document.querySelector(`.message`).textContent = `Correct Number!`;
+    document.querySelector(`.number`).textContent = ranNumber;
+
+    // manipulate css when there is a win
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).style.width = `30rem`;
+
+    // when there is a too low
+  } else if (guess > ranNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Lower`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `You Lost!`;
+    }
+
+    // when there is a too high
+  } else if (guess < ranNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Higher`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `You Lost!`;
+    }
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  score = ranNumber;
+  ranNumber = Math.trunc(Math.random() * maxNamber) + 1;
+
+  document.querySelector(`.messsage`).textContent = `Start guessing...`;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
 });
